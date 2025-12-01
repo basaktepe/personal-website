d064f9c61df87e307f7218ea07c64cb06e650ebf
Başak Tepe <basaktepe23@gmail.com>
2025-12-01 16:56:11 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+
+  return (
+    <div>
+     <h1 className="text-4xl font-bold text-pink-500">
+  Tailwind çalışıyor mu? 💅
+</h1>
+
+    </div>
+  )
+}
+
+export default App
